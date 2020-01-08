68a3ce08e660c301084ad334162ec142b2e2524b
Chad Saglam <saglam.chd@gmail.com>
2020-01-08 12:20:21 +0100

    Creating the database

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
+
 const PORT = process.env.PORT || 4001
 
 app.use(express.static('public'));
@@ -11,6 +14,28 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/strips', (req, res, next) => {
+  const strip = req.body.strip;
+  
+  db.run(`INSERT INTO Strip (head, body, bubble_type, background, bubble_text,caption)
+          VALUES ($head, $body, $bubbleType, $background, $bubbleText, $caption)`,
+    {
+      $head: strip.head,
+      $body: strip.body,
+      $bubbleType: strip.bubbleType,
+      $background: strip.background,
+      $bubbleText: strip.bubbleText,
+      $caption: strip.caption,
+    }, (err) => {
+      if (err) {
+        return res.sendStatus(500);
+      }
+    });
+});
+
+
 app.listen(PORT, 
   () => console.log(`Example app listening on port ${PORT}!`)
 );
+
+module.exports = app;
